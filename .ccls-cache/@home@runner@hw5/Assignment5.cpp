c88543844a9bedc43770005a2b13d976cf3402dc
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-02-23 00:24:31 +0000

    completes answer to part 1 of assignment

---
+++ .ccls-cache/@home@runner@hw5/Assignment5.cpp
@@ -0,0 +1,20 @@
+// Marc Jacques
+// Assignment 5 Part 1
+
+//a 10 20
+//b 20 10
+/*c
+  The swapValue method does not impact the variables 'i' or 'n' outside of the method. Because a copy of the value is passed and not the actual value this method creates a new object and stores the value in a different location in memory. The original value is unaffected.
+  
+The swapReference method passes a reference to the location where the actual value is stored in memory. Because of this we are actually manipulating the data that is stored in that memory location. Since the original vriable is referring to the same memory location any changes that occur in the method impacts the original variable.
+*/
+
+//Part 2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  return 0;
+}
--- .ccls-cache/@home@runner@hw5/main.cpp
+++ .ccls-cache/@home@runner@hw5/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+using namespace std;
+void swapValue(int, int);
+void swapReference(int &, int &);
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  int i =10,n = 20;
+  swapValue(i, n);
+  cout<<i <<"\t" << n<<endl; //---1
+ 
+  i =10,n = 20;
+  swapReference(i, n);
+  cout<<i <<"\t" << n<<endl; //---2
+  
+  return 0;
+}
+
+void swapValue(int a, int b)
+{
+   int temp = a;
+   a = b;
+   b = a;
+}
+
+void swapReference(int &a, int &b)
+{
+   int temp = a;
+   a = b;
+   b = temp;
 }
