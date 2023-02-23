c04aa603fb17596556299be343aab9c75504e013
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-02-23 03:41:00 +0000

    add inputValidation method

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -13,8 +13,75 @@ The swapReference method passes a reference to the location where the actual val
 #include <iostream>
 using namespace std;
 
+bool grade(char[], char[], int&, int&);
+char inputValidation(int, int&, char&);
 int main()
 {
+  const int SIZE = 10;
+  char correctAnswers[SIZE] = {'B', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D'};
+  char studentAnswers[SIZE];
+  int correct = 0, incorrect = 0;
+  bool pass;
   
+  for(int i = 0; i < 10; i++)
+    {
+      char answer;
+      int count = 0;
+      
+      cout << "Enter answer for #"<< i  + 1<<": ";
+      cin >> answer;
+      
+      studentAnswers[i] = inputValidation(i, count, answer);
+    }
+
+  pass = grade(studentAnswers, correctAnswers, correct, incorrect);
+  
+  if(pass == true) 
+  {
+    cout << "Congratulations!\nYou have passed exam.\nTotal number of correct answers: "<< correct<<"\nTotal number of incorrect answers: "<<incorrect;
+  } else {
+    cout << "Sorry, You have not passed the exam!\nTotal number of correct answers: "<< correct<<"\nTotal number of incorrect answers: "<<incorrect;
+  }
   return 0;
+}
+
+bool grade(char studentResponse[], char answerKey[], int& correct, int& incorrect)
+{
+
+  // for loop to iterate through each array
+  for(int i = 0; i < 10; i++)
+    {
+      // compare each corresponding value
+      if(studentResponse[i] != answerKey[i])
+      {
+        incorrect++;        
+      } else {
+        correct++;
+      }
+    }
+  return correct >= 8;
+}
+
+char inputValidation(int questionNumber, int& totalInvalidInputs, char& answer)
+{
+  //check totalinValid
+  while(totalInvalidInputs <= 3)
+    {
+      if(totalInvalidInputs >= 3)
+      {
+        cout << "GOOD BYE"<< endl;
+        abort();
+      }
+      if(answer < 65 || answer > 68)
+      {
+        totalInvalidInputs++;
+        cout << "The only valid inputs are 'A', 'B', 'C', or 'D'\n";
+        cout << "Enter answer for #"<< questionNumber  + 1<<": ";
+        cin >> answer;
+      } else {
+        return answer;
+      } 
+    }
+  
+  return answer;
 }
